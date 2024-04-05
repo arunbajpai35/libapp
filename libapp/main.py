963143d6b54a24728c0d075c35fb2a53cb645980
Arun <arunbajpai35@gmail.com>
2024-04-06 00:47:21 +0530

    Updated main config

--- libapp/main.py
+++ libapp/main.py
@@ -6,13 +6,14 @@ It sets up the FastAPI application and includes the router for the students API.
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 from api.v1.students import router as StudentRouter
 
 app = FastAPI()
 
+# CORS middleware
 origins = ['*']
-
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -21,4 +22,8 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Include routers
 app.include_router(StudentRouter)
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="0.0.0.0", port=8000)
